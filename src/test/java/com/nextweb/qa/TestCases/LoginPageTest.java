d791677a5756ffbb8f3ef72e26632ada335c5928
shubhvitthal <shubham.shubham168@gmail.com>
2022-08-19 12:02:13 +0530

    This is success of our team

--- src/test/java/com/nextweb/qa/TestCases/LoginPageTest.java
+++ src/test/java/com/nextweb/qa/TestCases/LoginPageTest.java
@@ -1,2 +1,5 @@
 //This is the login page functionality
 //write the code for the login page functionality shubh
+
+//ok it is done from my side you can review it now **
+//1st change in already exting file I done
---
+++ src/test/java/com/nextweb/qa/TestCases/MenshirtPageTest.java
@@ -0,0 +1,8 @@
+package com.nextweb.qa.TestCases;
+
+public class MenshirtPageTest {
+
+	//this is new test case for the men t shirts
+	
+	//this created myself in eclipse
+}
